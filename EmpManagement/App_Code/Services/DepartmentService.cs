e1c8d857e7c6ae2b35c0f6d2cae4238d6d953f9f
Harkirat Singh <honey.harkirat@outlook.com>
2021-12-30 15:07:10 +0530

    Department and Employee Services added.

--- EmpManagement/App_Code/Services/DepartmentService.cs
+++ EmpManagement/App_Code/Services/DepartmentService.cs
@@ -2,10 +2,119 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using EmpManagement.App_Code.Models;
 
 namespace EmpManagement.App_Code.Services
 {
-    public class DepartmentService
+    public static class DepartmentService
     {
+        private static readonly string connStr;
+        
+        static DepartmentService()
+        {
+            connStr = WebConfigurationManager.ConnectionStrings["DBConnectionStr"].ConnectionString;
+        }
+
+        public static void Delete(int id)
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("DELETE FROM DEPARTMENTS WHERE ID = @ID", connection);
+            command.Parameters.AddWithValue("ID", id);
+            using (connection)
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public static void Update(Department department)
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("UPDATE DEPARTMENTS SET NAME = @NAME, DESCRIPTION = @DESCRIPTION WHERE ID = @ID", connection);
+            command.Parameters.AddWithValue("ID", department.Id);
+            command.Parameters.AddWithValue("NAME", department.Name);
+            command.Parameters.AddWithValue("DESCRIPTION", department.Description);
+            using (connection)
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public static void Insert(Department department)
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("INSERT INTO DEPARTMENTS VALUES(@ID, @NAME, @DESCRIPTION)", connection);
+            command.Parameters.AddWithValue("ID", GetNumberOfDepartments() + 1);
+            command.Parameters.AddWithValue("NAME", department.Name);
+            command.Parameters.AddWithValue("DESCRIPTION", department.Description);
+            using (connection)
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public static int GetNumberOfDepartments()
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("SELECT Count(*) FROM DEPARTMENTS", connection);
+            int count = 0;
+            using (connection)
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                count = reader.GetInt32(0);
+            }
+            return count;
+        }
+
+        public static Department GetDepartmentById(int id)
+        {
+            Department department;
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("SELECT * FROM DEPARTMENTS WHERE ID = @ID", connection);
+            command.Parameters.AddWithValue("ID", id);
+            using (connection)
+            {
+                connection.Open();
+                var reader = command.ExecuteReader();
+                reader.Read();
+                department = new Department()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    Description = reader["Description"].ToString()
+                };
+            }
+
+            return department;
+        }
+
+        public static List<Department> GetAll()
+        {
+            var departmentList = new List<Department>();
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("SELECT * FROM DEPARTMENTS", connection);
+            using (connection)
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Department department = new Department()
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Name = reader["Name"].ToString(),
+                        Description = reader["Description"].ToString()
+                    };
+                    departmentList.Add(department);
+                }
+            }
+            return departmentList;
+        }
     }
 }
---
+++ EmpManagement/App_Code/Services/EmployeeService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using EmpManagement.App_Code.Models;
+
+namespace EmpManagement.App_Code.Services
+{
+    public static class EmployeeService
+    {
+        private static readonly string connStr;
+        
+        static EmployeeService()
+        {
+            connStr = WebConfigurationManager.ConnectionStrings["DBConnectionStr"].ConnectionString;
+        }
+
+        public static void Delete(int id)
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("DELETE FROM EMPLOYEES WHERE ID = @ID", connection);
+            command.Parameters.AddWithValue("ID", id);
+            using (connection)
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public static void Update(Employee employee)
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("UPDATE EMPLOYEES SET NAME = @NAME, ROLE = @ROLE, " +
+                "ADDRESS = @ADDRESS, JOINED = @JOINED, DEPARTMENT_ID = @DEPARTMENT_ID WHERE ID = @ID", connection);
+            command.Parameters.AddWithValue("ID", employee.Id);
+            command.Parameters.AddWithValue("NAME", employee.Name);
+            command.Parameters.AddWithValue("ROLE", employee.Role);
+            command.Parameters.AddWithValue("ADDRESS", employee.Address);
+            command.Parameters.AddWithValue("JOINED", employee.Joined);
+            command.Parameters.AddWithValue("DEPARTMENT_ID", employee.Department.Id);
+            using (connection)
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public static void Insert(Employee employee)
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("INSERT INTO EMPLOYEES VALUES(@ID, @NAME, @ROLE, @ADDRESS, @JOINED, @DEPARTMENT_ID)", connection);
+            command.Parameters.AddWithValue("ID", GetNumberOfEmployees() + 1);
+            command.Parameters.AddWithValue("NAME", employee.Name);
+            command.Parameters.AddWithValue("ROLE", employee.Role);
+            command.Parameters.AddWithValue("ADDRESS", employee.Address);
+            command.Parameters.AddWithValue("JOINED", employee.Joined);
+            command.Parameters.AddWithValue("DEPARTMENT_ID", employee.Department.Id);
+            using (connection)
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public static int GetNumberOfEmployees()
+        {
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("SELECT Count(*) FROM EMPLOYEES", connection);
+            int count = 0;
+            using (connection)
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                count = reader.GetInt32(0);
+            }
+            return count;
+        }
+
+        public static List<Employee> GetAll()
+        {
+            var employees = new List<Employee>();
+            var connection = new SqlConnection(connStr);
+            var command = new SqlCommand("SELECT * FROM EMPLOYEES", connection);
+            using (connection)
+            {
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var employee = new Employee() 
+                    {
+                        Id = Convert.ToInt32(reader["ID"]),
+                        Name = reader["Name"].ToString(),
+                        Address = reader["Address"].ToString(),
+                        Department = DepartmentService.GetDepartmentById(Convert.ToInt32(reader["Department_Id"])),
+                        Joined = DateTime.Parse(reader["Joined"].ToString()),
+                        Role = (Role)Convert.ToInt32(reader["Role"])
+                    };
+                    employees.Add(employee);
+                }
+            }
+            return employees;
+        }
+    }
+}
